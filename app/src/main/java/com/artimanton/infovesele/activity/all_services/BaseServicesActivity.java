7da1a42d67da6c96761e7e33314bac6d28e7f627
artim-anton <artim-anton@yandex.ru>
2018-06-07 22:42:34 +0300

    add Services

--- app/src/main/java/com/artimanton/infovesele/activity/all_services/BaseServicesActivity.java
+++ app/src/main/java/com/artimanton/infovesele/activity/all_services/BaseServicesActivity.java
@@ -1,61 +1,32 @@
 package com.artimanton.infovesele.activity.all_services;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
 
-import com.artimanton.infovesele.R;
 import com.artimanton.infovesele.adapters.ServicesAdapter;
 import com.artimanton.infovesele.model.ServicesModel;
-import com.artimanton.infovesele.permission.Internet;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class BeautyActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private List<ServicesModel> result;
-    private ServicesAdapter adapter;
-    private Button btnPushToServer;
+public class BaseServicesActivity extends AppCompatActivity{
+    private  List<ServicesModel> result;
+    private  ServicesAdapter adapter;
+    private  DatabaseReference reference;
 
-    private FirebaseDatabase database;
-    private DatabaseReference reference;
+    public BaseServicesActivity() {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_beauty);
-        if (!Internet.isOnline(this)){
-            Toast.makeText(this, "Проверьте подключение к Интернету", Toast.LENGTH_LONG).show();
-        }
+    }
 
-        btnPushToServer = (Button) findViewById(R.id.btn_push_to_server);
-
-        database = FirebaseDatabase.getInstance();
-        reference = database.getReference("services/beauty");
-
-        result = new ArrayList<>();
-        recyclerView =  findViewById(R.id.services_list);
-        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
-        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
-        recyclerView.setLayoutManager(layoutManager);
-
-        adapter = new ServicesAdapter(result);
-        recyclerView.setAdapter(adapter);
-
-        updateList();
-
+    public BaseServicesActivity(List<ServicesModel> result, ServicesAdapter adapter, DatabaseReference reference) {
+        this.result = result;
+        this.adapter = adapter;
+        this.reference = reference;
     }
     //Context menu
    /*  @Override
@@ -72,7 +43,7 @@ public class BeautyActivity extends AppCompatActivity {
         return super.onContextItemSelected(item);
     }*/
 
-    protected void updateList(){
+    public void updateList(){
         reference.addChildEventListener(new ChildEventListener() {
             @Override
             public void onChildAdded(DataSnapshot dataSnapshot, String s) {
@@ -136,5 +107,4 @@ public class BeautyActivity extends AppCompatActivity {
     public void pushToServer(View view) {
         changeServices(ServicesAdapter.getAdapterPosition());
     }
-
 }
--- app/src/main/java/com/artimanton/infovesele/activity/all_services/BuilderActivity.java
+++ app/src/main/java/com/artimanton/infovesele/activity/all_services/BuilderActivity.java
@@ -2,14 +2,53 @@ package com.artimanton.infovesele.activity.all_services;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.artimanton.infovesele.R;
+import com.artimanton.infovesele.adapters.ServicesAdapter;
+import com.artimanton.infovesele.model.ServicesModel;
+import com.artimanton.infovesele.permission.Internet;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
-public class BuilderActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class BuilderActivity extends BaseServicesActivity{
+    private RecyclerView recyclerView;
+    private List<ServicesModel> result;
+    private ServicesAdapter adapter;
+    private Button btnPushToServer;
+
+    private FirebaseDatabase database;
+    private DatabaseReference reference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_builder);
+        if (!Internet.isOnline(this)){
+            Toast.makeText(this, "Проверьте подключение к Интернету", Toast.LENGTH_LONG).show();
+        }
+
+        btnPushToServer = (Button) findViewById(R.id.btn_push_to_server);
+
+        database = FirebaseDatabase.getInstance();
+        reference = database.getReference("services/builder");
+
+        result = new ArrayList<>();
+        recyclerView =  findViewById(R.id.services_list);
+        LinearLayoutManager layoutManager = new LinearLayoutManager(this);
+        layoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(layoutManager);
+
+        adapter = new ServicesAdapter(result);
+        recyclerView.setAdapter(adapter);
+
+        BaseServicesActivity baseServicesActivity = new BaseServicesActivity(result,adapter,reference);
+        baseServicesActivity.updateList();
     }
 }
