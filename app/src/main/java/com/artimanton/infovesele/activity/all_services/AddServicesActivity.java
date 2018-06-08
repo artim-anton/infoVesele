23cc2f7037c95dd3ad7ebfadf94c7c272283d0cd
artim-anton <artim-anton@yandex.ru>
2018-06-08 11:54:45 +0300

    add Anim

--- app/src/main/java/com/artimanton/infovesele/activity/all_services/AddServicesActivity.java
+++ app/src/main/java/com/artimanton/infovesele/activity/all_services/AddServicesActivity.java
@@ -1,13 +1,20 @@
 package com.artimanton.infovesele.activity.all_services;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.EditText;
 
 import com.artimanton.infovesele.R;
 
 public class AddServicesActivity extends AppCompatActivity {
+    EditText etPhoneReclame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,5 +25,22 @@ public class AddServicesActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setDisplayShowHomeEnabled(true);
 
+        etPhoneReclame = (EditText) findViewById(R.id.et_phone_reclame);
+    }
+
+    public void btnCallReclame(View view) {
+        String phone = etPhoneReclame.getText().toString();
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.fromParts("tel", phone, null));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(intent);
     }
 }
