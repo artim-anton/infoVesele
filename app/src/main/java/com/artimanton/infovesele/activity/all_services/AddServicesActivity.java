45e046c4f4ffbba75db28cb89312547214764495
artim-anton <artim-anton@yandex.ru>
2018-06-10 18:48:43 +0300

    fix bag

--- app/src/main/java/com/artimanton/infovesele/activity/all_services/AddServicesActivity.java
+++ app/src/main/java/com/artimanton/infovesele/activity/all_services/AddServicesActivity.java
@@ -1,6 +1,11 @@
 package com.artimanton.infovesele.activity.all_services;
 
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
@@ -9,14 +14,41 @@ import com.artimanton.infovesele.R;
 
 public class AddServicesActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_services);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
+
+        /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);*/
+
+
+
+    }
+
+
+    public void backButton(View view) {
+        finish();
+    }
+
+    public void btnCallReclame(View view) {
+        String phone = "0978792587";
+        Intent intent = new Intent(Intent.ACTION_CALL, Uri.fromParts("tel", phone, null));
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+        startActivity(intent);
 
     }
 }
