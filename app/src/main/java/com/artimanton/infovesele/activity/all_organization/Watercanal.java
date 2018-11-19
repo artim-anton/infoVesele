c197b9c40de094df0bdc1cba692f493267be56b0
Anton <artim-anton@yandex.ru>
2018-11-19 22:39:05 +0200

    change logo, add button renewNotification

--- app/src/main/java/com/artimanton/infovesele/activity/all_organization/Watercanal.java
+++ app/src/main/java/com/artimanton/infovesele/activity/all_organization/Watercanal.java
@@ -1,5 +1,7 @@
 package com.artimanton.infovesele.activity.all_organization;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
@@ -7,7 +9,10 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.artimanton.infovesele.R;
+import com.artimanton.infovesele.notification.Notification_reciever;
 import com.artimanton.infovesele.utilities.BackGroundActivity;
+
+import java.util.Calendar;
 
 public class Watercanal extends AppCompatActivity {
 
@@ -32,5 +37,22 @@ public class Watercanal extends AppCompatActivity {
         intent.addCategory(Intent.CATEGORY_BROWSABLE);
         intent.setData(Uri.parse("http://vodokanalvesele.at.ua"));
         startActivity(intent);
+    }
+
+    public void btnCreateNotification(View view) {
+        Calendar calendar = Calendar.getInstance();
+
+        calendar.set(Calendar.DAY_OF_MONTH, 20);
+        calendar.set(Calendar.HOUR_OF_DAY, 19);
+        calendar.set(Calendar.MINUTE, 30);
+
+        Intent intent = new Intent(getApplicationContext(), Notification_reciever.class);
+        intent.setAction("MY_NOTIFICATION_MESSAGE");
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(getApplicationContext(), 100, intent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        alarmManager.cancel(pendingIntent);
+        alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(), AlarmManager.INTERVAL_DAY*31, pendingIntent);
     }
 }
